cf2efd9c4d936cd94f9eac6ba468ce9045aa814e
Pankaj Joshi <Pankaj.Joshi.hp@gmail.com>
2025-03-18 18:47:49 +0530

    Created Shorebird CLI setup and Integrated Shorebird using Shorebird CLI and shorebird_code_push

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shorebird_code_push/shorebird_code_push.dart';
 
 void main() {
-  runApp(MyApp());
-  // print("Hey this is main page");
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,21 +11,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _updater = ShorebirdUpdater();
+  Patch? _currentPatch;
+
+  @override
+  void initState() {
+    super.initState();
+    _updater.readCurrentPatch().then((patch) {
+      setState(() => _currentPatch = patch);
+    });
+  }
+
+  Future<void> _checkForUpdate() async {
+    final status = await _updater.checkForUpdate();
+    if (status == UpdateStatus.outdated) {
+      await _updater.update();
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Center(
-        child: Text("Hey this is home page"),
-      )),
+      appBar: AppBar(title: const Text('Shorebird Code Push')),
+      body: Center(
+        child: Text(
+          'Patch: ${_currentPatch?.number ?? "None"}',
+          style: const TextStyle(fontSize: 18),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _checkForUpdate,
+        child: const Icon(Icons.refresh),
+      ),
     );
   }
 }

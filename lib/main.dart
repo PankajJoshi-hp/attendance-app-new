f24d0f97231f0eddade1ac6abd2ad6dead6c2a88
Pankaj Joshi <Pankaj.Joshi.hp@gmail.com>
2025-03-19 17:48:08 +0530

    Changed the structure of flutter.yml

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Shorebird Code Push Applicaion')),
+      appBar: AppBar(title: const Text('Shorebird Code Push Application ')),
       body: Center(
         child: Text(
           'Patch: ${_currentPatch?.number ?? "None"}',
